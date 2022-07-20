0d679d251f16cd7ba679423ed1896ca26147a5d9
defaultkavy <defaultkavy@gmail.com>
2022-07-20 13:28:00 +0800

    Rebuild js files

--- js/ts/Base.js
+++
@@ -1,6 +0,0 @@
-export class Base {
-    constructor(client) {
-        this.client = client;
-    }
-}
-//# sourceMappingURL=Base.js.map
--- js/ts/console.js
+++
@@ -1,4 +0,0 @@
-import { Client } from "./Client.js";
-window.client = new Client();
-document.body.style.backgroundColor = '#36393f';
-//# sourceMappingURL=console.js.map
